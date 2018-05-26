f69a3569bfaa18abb3f1f273235df3ba07050e23
nodnarb-f <39176001+nodnarb-f@users.noreply.github.com>
2018-05-26 14:27:09 -0400

    Add files via upload

---
+++ reverseastring.js
@@ -0,0 +1,15 @@
+var result = "";
+
+function reverseIt(input) {
+
+  var i = input.length;
+  for (var i = input.length - 1; i >= 0; i--) {
+    result += input.charAt(i);;
+  }
+  return result;
+
+}
+var input = prompt("Enter a string: ","");
+
+console.log(reverseIt(input));
+document.write(result);
